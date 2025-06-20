a21cbfd177e07ff5da80600f4f9165ebe48a9196
Nargiz <skakovasaule7@gmail.com>
2025-06-20 23:18:32 +0500

    k

---
+++ migrations/init.sql
@@ -0,0 +1,8 @@
+CREATE TABLE aggregated_trading_data (
+    pair_name TEXT NOT NULL,
+    exchange TEXT NOT NULL,
+    timestamp TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+    average_price DOUBLE PRECISION NOT NULL,
+    min_price DOUBLE PRECISION NOT NULL,
+    max_price DOUBLE PRECISION NOT NULL
+);
